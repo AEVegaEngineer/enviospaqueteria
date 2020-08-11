52c54210ebdaa83eac306db26b3377bdbfbd8348
Sulfur0 <aevega1991@gmail.com>
2020-08-10 22:37:59 -0300

    Arreglado formato del contrato, falta link al registro

--- resources/views/contrato/index.blade.php
+++ resources/views/contrato/index.blade.php
@@ -5,35 +5,33 @@
 <!-- ======= Alertas ======= -->
 @include('alerts.alerts')
 
-<!-- ======= Hero Section ======= -->
+<main id="main">
 
-<section id="hero" class="d-flex align-items-center">
-  <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
-    <div class="row justify-content-center" id="banner-inicio">
-      <div class="col-xl-7 col-lg-9 text-center mt-4">
-        <h1>Términos y Condiciones</h1>
-        <h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat lobortis augue. Aliquam lacinia facilisis auctor. Nam dapibus faucibus ex, non cursus neque porttitor vel. Pellentesque facilisis arcu non hendrerit hendrerit. Aliquam maximus diam sit amet erat porttitor venenatis. Nunc fringilla venenatis nunc at ullamcorper. Vivamus ultrices accumsan turpis eget efficitur. Etiam mollis aliquam nisl nec convallis. Ut fringilla metus ac sapien pellentesque blandit.
+  <!-- ======= Pricing Section ======= -->
+  
+  <section id="pricing" class="pricing">
+    <div class="container" data-aos="fade-up">
 
-Vivamus maximus, erat vel dictum luctus, lorem dolor congue odio, eu porta nunc lacus vitae quam. Quisque ultrices, enim vel iaculis pellentesque, dolor lacus vehicula neque, vitae convallis tortor diam quis nibh. Sed in porta nulla. Integer in sem eget orci placerat finibus. Proin eleifend risus non lorem gravida, et semper felis porta. Etiam in lobortis libero. Morbi id malesuada velit. Nunc placerat suscipit magna vitae euismod. Maecenas semper ut risus quis molestie. Vestibulum vestibulum ligula mauris, et tempor lectus molestie sit amet.
-
-Pellentesque justo augue, eleifend et volutpat ut, iaculis non purus. Donec non libero rutrum, pulvinar lacus et, semper nibh. Nam felis est, dictum non turpis vitae, vehicula elementum arcu. Vivamus ultrices ante quam, rutrum scelerisque velit vulputate sed. Vestibulum vehicula ante eu risus porta volutpat. Maecenas mattis risus placerat dictum feugiat. Quisque nec nunc sit amet mi tincidunt sollicitudin. Phasellus malesuada libero lectus. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean facilisis, purus a consequat bibendum, purus nisl laoreet risus, vitae laoreet libero erat eget lorem.
-
-Suspendisse potenti. Aliquam nisi lectus, dictum eu elementum et, porta vitae mauris. Vivamus ac quam vitae risus consectetur volutpat id nec nibh. In suscipit nec leo in tincidunt. Donec semper convallis vestibulum. Mauris efficitur felis justo, vel lacinia neque bibendum in. Nunc ultricies faucibus augue ut luctus. Aenean eu sem maximus, tempor nulla non, semper ante. Integer luctus laoreet volutpat. Aenean sit amet condimentum ante. Nullam blandit massa sit amet faucibus commodo. Pellentesque ultrices sagittis consectetur. Nullam ultrices congue orci vel porta. Duis elementum posuere odio, non malesuada nulla mattis sed. Nullam augue arcu, bibendum tempus sem iaculis, efficitur ultrices massa. In molestie ut quam id pretium.
-
-Phasellus egestas diam velit, a condimentum arcu mollis sed. Mauris vel euismod risus. Fusce et ex quis ipsum suscipit efficitur. Aenean porttitor, nisl in tincidunt tincidunt, elit ipsum condimentum purus, id vehicula urna libero at leo. Sed ac aliquet ex, ut pulvinar diam. Sed a porta erat. Proin et nisi rutrum, tincidunt justo id, feugiat orci. Mauris blandit at elit tristique venenatis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque semper facilisis dictum.
-        </h2>
-        <h2><b>Al hacer click en el siguiente vínculo accederá a la página de registro y aceptará todos los términos y condiciones aquí descritos</b></h2>
-
+      <div class="section-title">
+        <h2>Términos y Condiciones</h2>
+        <p>Lee cuidadosamente estos términos y condiciones, acepta el contrato al final para poder contratar nuestros servicios</p>
       </div>
+
+      <div class="row">
+
+        <div class="col-12" data-aos="zoom-im" data-aos-delay="100">
+          <div class="box text-justify">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat lobortis augue. Aliquam lacinia facilisis auctor. Nam dapibus faucibus ex, non cursus neque porttitor vel. Pellentesque facilisis arcu non hendrerit hendrerit. Aliquam maximus diam sit amet erat porttitor venenatis. Nunc fringilla venenatis nunc at ullamcorper. Vivamus ultrices accumsan turpis eget efficitur. Etiam mollis aliquam nisl nec convallis. Ut fringilla metus ac sapien pellentesque blandit.</p>
+            <p>Vivamus maximus, erat vel dictum luctus, lorem dolor congue odio, eu porta nunc lacus vitae quam. Quisque ultrices, enim vel iaculis pellentesque, dolor lacus vehicula neque, vitae convallis tortor diam quis nibh. Sed in porta nulla. Integer in sem eget orci placerat finibus. Proin eleifend risus non lorem gravida, et semper felis porta. Etiam in lobortis libero. Morbi id malesuada velit. Nunc placerat suscipit magna vitae euismod. Maecenas semper ut risus quis molestie. Vestibulum vestibulum ligula mauris, et tempor lectus molestie sit amet.</p>
+            <p>Pellentesque justo augue, eleifend et volutpat ut, iaculis non purus. Donec non libero rutrum, pulvinar lacus et, semper nibh. Nam felis est, dictum non turpis vitae, vehicula elementum arcu. Vivamus ultrices ante quam, rutrum scelerisque velit vulputate sed. Vestibulum vehicula ante eu risus porta volutpat. Maecenas mattis risus placerat dictum feugiat. Quisque nec nunc sit amet mi tincidunt sollicitudin. Phasellus malesuada libero lectus. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean facilisis, purus a consequat bibendum, purus nisl laoreet risus, vitae laoreet libero erat eget lorem.</p>
+            <p>Suspendisse potenti. Aliquam nisi lectus, dictum eu elementum et, porta vitae mauris. Vivamus ac quam vitae risus consectetur volutpat id nec nibh. In suscipit nec leo in tincidunt. Donec semper convallis vestibulum. Mauris efficitur felis justo, vel lacinia neque bibendum in. Nunc ultricies faucibus augue ut luctus. Aenean eu sem maximus, tempor nulla non, semper ante. Integer luctus laoreet volutpat. Aenean sit amet condimentum ante. Nullam blandit massa sit amet faucibus commodo. Pellentesque ultrices sagittis consectetur. Nullam ultrices congue orci vel porta. Duis elementum posuere odio, non malesuada nulla mattis sed. Nullam augue arcu, bibendum tempus sem iaculis, efficitur ultrices massa. In molestie ut quam id pretium.</p>
+            <p>Phasellus egestas diam velit, a condimentum arcu mollis sed. Mauris vel euismod risus. Fusce et ex quis ipsum suscipit efficitur. Aenean porttitor, nisl in tincidunt tincidunt, elit ipsum condimentum purus, id vehicula urna libero at leo. Sed ac aliquet ex, ut pulvinar diam. Sed a porta erat. Proin et nisi rutrum, tincidunt justo id, feugiat orci. Mauris blandit at elit tristique venenatis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque semper facilisis dictum.</p>
+            <p><b>Al hacer click en el siguiente vínculo accederá a la página de registro y aceptará todos los términos y condiciones aquí descritos</b></p>
+          </div>
+        </div>
+        
+      </div>   
     </div>
-    <div class="text-center">
-      <a href="/register" class="btn-get-started scrollto text-decoration-none">Siguiente</a>
-    </div>    
-  </div>
-</section>
-
-<!-- End Hero -->
-
-</main><!-- End #main -->
+  </section>
+</div>
 @endsection
